918eb1fd7f190dd0092d7a9caa6f962fff5ebb8a
Maxim <syromolotov.maks@mail.ru>
2020-03-15 13:19:17 +0300

    First version

---
+++ Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace Password_GUI_Generator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+
+        private static string str()
+        {
+            string s = "";
+            for (char c = 'A'; c <= 'Z'; c++)
+            {
+                s += c;
+            }
+
+            for (char c = 'a'; c < 'z'; c++)
+            {
+                s += c;
+            }
+
+            for (char c = '1'; c < '9'; c++)
+            {
+                s += c;
+            }
+
+            s += "+-*/!@#$%^&*()_+~\\|{}<>?.;:,";
+            return s;
+        }
+
+        public string gen_password()
+        {
+            return null;
+        }
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.Bisque;
+            this.textBox1.Location = new System.Drawing.Point(12, 23);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(548, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Tan;
+            this.label1.Location = new System.Drawing.Point(9, 6);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "password:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Khaki;
+            this.button1.Location = new System.Drawing.Point(12, 72);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(548, 102);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.MouseEnter += new System.EventHandler(this.button1_MouseEnter_1);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.BackColor = System.Drawing.Color.Ivory;
+            this.checkBox1.Location = new System.Drawing.Point(16, 49);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(48, 17);
+            this.checkBox1.TabIndex = 3;
+            this.checkBox1.Text = "Hide";
+            this.checkBox1.UseVisualStyleBackColor = false;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(102, 49);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(99, 17);
+            this.checkBox2.TabIndex = 4;
+            this.checkBox2.Text = "Change Length";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.AutoCompleteCustomSource.AddRange(new string[] {
+            "Enter Length Here:"});
+            this.textBox2.Location = new System.Drawing.Point(254, 47);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(133, 20);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.Text = "Enter here length";
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            this.textBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox2_KeyPress);
+            this.textBox2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.textBox2_MouseDown);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.BackgroundImage = global::Password_GUI_Generator.Properties.Resources.mamba;
+            this.ClientSize = new System.Drawing.Size(640, 451);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Password_GUI_Generator
+{
+    public partial class Form1 : Form
+    {
+        private void button1_MouseEnter(object sender, EventArgs e)
+        {
+            //Создаём новый экземпляр генератора псевдослучайных чисел
+            Random rnd = new Random();
+            //Перемещаем кнопку на случайную позицию
+            button1.Location = new Point(
+                rnd.Next(0, this.Width - 50),
+                rnd.Next(0, this.Height - 50));
+        }
+
+        private readonly string _all = str();
+
+        string GenRandomString(string Alphabet, int Length)
+        {
+            Random rnd = new Random();
+            string x = "";
+            for (int i = 0; i < Length; i++)
+            {
+                var Position = rnd.Next(0, Alphabet.Length - 1);
+                x += Alphabet[Position];
+            }
+
+
+            return x;
+        }
+
+        public void check()
+        {
+            if (textBox1.Text == "")
+            {
+                button1.Text = @"gen password";
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            textBox2.Hide();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = @"Hello";
+            textBox1.Text = GenRandomString(_all, _x);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            button1.Text = @"Your password is " + textBox1.Text;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            label1.Text = @"Hail!!!";
+            textBox1.Text = "";
+            check();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            string bt = button1.Text;
+            button1.Visible = false;
+            checkBox1.Text = @"Show";
+            string text = textBox1.Text;
+            if (checkBox1.Checked == true)
+            {
+                textBox1.Hide();
+                checkBox1.Text = @"Show";
+                //  textBox1.Show();
+
+            }
+
+            //    button1.Text = GenRandomString(_all,15);
+            //  System.Threading.Thread.Sleep(1000);
+            button1.Text = @"";
+            if (checkBox1.Checked != true)
+            {
+                button1.Visible = true;
+                textBox1.Show();
+                checkBox1.Text = @"Hide";
+
+            }
+
+            button1.Text = bt;
+
+            textBox1.Text = text;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            textBox2.Show();
+            if (!checkBox2.Checked)
+            {
+                textBox2.Hide();
+            }
+        }
+
+        public static int _x = 15;
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            //_x = (textBox2.Text != "") ? int.Parse(textBox2.Text) :
+            //    (textBox2.Text == "") ? 0 : int.Parse(textBox2.Text + "\b");
+
+            _x = (textBox2.Text=="")?0:int.Parse(textBox2.Text);
+        }
+
+
+        const int maximum = 1000;
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if ((e.KeyChar >= '0' && e.KeyChar <= '9') || e.KeyChar==(char) Keys.Back/*|| e.KeyChar == ',' || e.KeyChar == '.'*/)
+            {
+            }
+            else
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void textBox2_MouseDown(object sender, MouseEventArgs e)
+        {
+            this.textBox2.Text = "";
+        }
+
+        private void button1_MouseEnter_1(object sender, EventArgs e)
+        {
+            //Создаём новый экземпляр генератора псевдослучайных чисел
+            Random rnd = new Random();
+            //Перемещаем кнопку на случайную позицию
+            button1.Location = new Point(
+                rnd.Next(0, this.Width - 50),
+                rnd.Next(0, this.Height - 50));
+        }
+
+        //public void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        //{
+        //    //// символ - число
+        //    bool isNumber = char.IsNumber(e.KeyChar);
+        //    //if (isNumber)
+        //    //{
+        //    //    // текстовое поле, на котором сработало событие
+        //    //    TextBox tb = sender as TextBox;
+        //    //    // Если введёное число в сочетании с текущим текстом в поле
+        //    //    // болььше или равно максимальному, запрещаем вводить
+        //    //    e.Handled = int.Parse(tb.Text + e.KeyChar.ToString()) >= maximum;
+        //    //}
+        //   // else
+        //        // запрещаем ввод любого не числа и не управляющего символа (tab, del, backspace и т.п.)
+        //        e.Handled = !(isNumber || char.IsControl(e.KeyChar));
+        //}
+
+        //private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        //{
+        //    if ((e.KeyChar >= '1' && e.KeyChar <= '9') /*|| e.KeyChar == ',' || e.KeyChar == '.'*/)
+        //    {
+        //    }
+        //    else
+        //    {
+        //        e.Handled = true;
+        //    }
+        //}
+    }
+}
